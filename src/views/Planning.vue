47b2f200cdf1f0cf0222c4670cf4a3b8b6f4e132
Matteo Caravati <matteo.caravati@protonmail.com>
2022-03-14 17:14:48 +0100

    (feat) Refactored Lesson into Planning

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="planning-view">
+    <div class="planning-view" v-if="!displayingLesson">
       <section>
         <span class="button" @click="getPreviousDay"><img src="../assets/chevron-left-solid.svg" alt="chevron-left"></span>
         <h4 v-if="planning_data">{{ this.planning_data.utc }}</h4>
         <span class="button" @click="getNextDay"><img src="../assets/chevron-right-solid.svg" alt="chevron-right"></span>
       </section>
 
-      <div class="planning-wrapper" v-if="planning_data">
+      <div class="planning-view" v-if="planning_data">
           <div
               v-for="value in this.planning_data.events"
               class="event"
@@ -15,6 +15,26 @@
             <h6>{{ value.start }} - {{ value.end }}</h6>
             <h4>{{ value.summary }}</h4>
           </div>
+      </div>
+    </div>
+
+    <div class="lesson-wrapper" v-if="displayingLesson">
+      <div class="lesson" @click="hideLesson">
+        <h6>{{ getLessonType() || "Inconnu" }}</h6>
+        <h4>{{ lesson.start }} - {{ lesson.end }}</h4>
+        <h2>{{ lesson.summary }}</h2>
+
+        <article class="teachers">
+          <h6 v-for="teacher in getTeachers()">
+            {{ teacher }}
+          </h6>
+        </article>
+
+        <article class="rooms">
+          <h6 v-for="room in getRooms()">
+            {{ room }}
+          </h6>
+        </article>
       </div>
     </div>
 </template>
@@ -34,7 +54,11 @@ export default {
       currentObjectIndex: 0,
       moment: moment(),
       dayIndex: -1,
-      user: null
+
+      user: null,
+
+      displayingLesson: false,
+      lesson: null
     }
   },
   async mounted() {
@@ -119,8 +143,43 @@ export default {
       this.planning_data = this.cache[this.dayIndex];
     },
     displayLesson(value) {
-      this.$store.dispatch("setSelectedLesson", value);
-      this.$router.push("lesson");
+      this.lesson = value;
+      this.displayingLesson = true;
+    },
+    getLessonType() {
+      return this.lesson.description.split("\n\n")[1].split("\n")[0];
+    },
+    getTeachers() {
+      return this.lesson.description.trim().split("\n").filter(this.isName);
+    },
+    isName(string) {
+      let isName = true;
+      const tokens = string.split(" ");
+      const containsNumber = string => /\d/.test(string);
+
+      for(let i = 0; i < tokens.length - 2; i++) {
+        if ((! isName))
+          return false;
+
+        isName = (tokens[i]).toUpperCase() === tokens[i];
+      }
+
+      const lastToken = tokens[tokens.length - 1];
+      const isFirstLetterUppercase = lastToken.charAt(0) === lastToken.charAt(0).toUpperCase();
+      const isSecondLetterLowercase = lastToken.charAt(1) === lastToken.charAt(1).toLowerCase();
+
+      return isName
+          && isFirstLetterUppercase
+          && isSecondLetterLowercase
+          && tokens.length >= 2
+          && (!containsNumber(string));
+    },
+    getRooms() {
+      return this.lesson.location.replaceAll("\\", "").split(",");
+    },
+    hideLesson() {
+      this.lesson = null;
+      this.displayingLesson = false;
     }
   }
 }
@@ -184,13 +243,78 @@ section {
   width: 80vw;
 }
 
-h6 {
+.planning-view h6 {
   margin-bottom: 0;
   margin-top: 0.5em;
 }
 
-h4 {
+.planning-view h4 {
   margin-top: 0.5em;
   margin-bottom: 0.5em;
+}
+
+.lesson {
+  background-color: white;
+  border-radius: 15px;
+  height: 90%;
+  width: 80%;
+  padding: 1vh 5vw;
+  word-break: break-word;
+  text-align: start;
+  box-sizing: border-box;
+}
+
+.lesson h6 {
+  background-color: #FEEAEA;
+  width: fit-content;
+  padding: 1% 5%;
+  border-radius: 30px;
+  color: #FC3D3E;
+}
+
+.lesson h4 {
+  margin-bottom: 0;
+}
+
+.teachers {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  max-height: 27%;
+}
+
+.teachers > h6 {
+  margin: 1% 0;
+  color: #56B377;
+  background-color: #EAF6EE;
+}
+
+.rooms {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  max-height: 27%;
+}
+
+.rooms > h6 {
+  margin: 1% 0;
+  color: #FE904A;
+  background-color: #FFF2E5;
+}
+
+article > h6 {
+  font-size: 0.6em;
+}
+
+article {
+  margin-bottom: 5%;
+}
+
+.lesson-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
 }
 </style>
